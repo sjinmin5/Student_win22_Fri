2d427592d6cd012329258603cbe3ce8a43721190
Suhjin Min <minsuh@sheridancollege.ca>
2022-01-21 09:35:12 -0500

    first commit

---
+++ src/student/Student1.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package student;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Suhjin Min
+ */
+public class Student1 {
+    private String studentId;
+    private String studentName;
+
+    public Student1(String studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    
+
+}
---
+++ src/student/StudentList.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package student;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Suhjin Min
+ */
+public class StudentList {
+    public static void main(String[] args){
+        
+        Student1[] studentList = new Student1[3];
+        
+        studentList[0] = new Student1("s1", "Ronak Sheth");
+        studentList[1] = new Student1("s2", "john doe");
+        studentList[2] = new Student1("s3", "abc");
+        
+        for(int i=0;i<studentList.length;i++){
+            System.out.println(studentList[i].getStudentName());
+        }
+    
+    }
+
+}
